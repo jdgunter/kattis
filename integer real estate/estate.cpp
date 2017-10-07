8dd772c320d8167a8a78a7a55a74312837ac6213
Joshua Gunter <joshua.d.gunter@gmail.com>
2017-10-07 17:01:35 -0300

    Update estate.cpp

--- integer real estate/estate.cpp
+++ integer real estate/estate.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+/*
+If a customer can purchase 'k' consecutive houses, starting at house 'a', that means
+  (a + 1) + (a + 2) + (a + 3) + ... (a + k) = n
+which simplifies to
+  ka + k(k+1)/2 = n
+Let's rearrange this equation so we can solve for 'a':
+  a = (2n - k(k+1))/2k
+As long as 'a' is an integer, we know there's a way to purchase 'k' consecutive houses. We don't need to know what the value of 'a'
+actually is. This means we only need to check whether
+  2k | 2n - k(k+1)
+in other words, does
+  2n - k(k+1) = 0 (mod 2k)
+hold. If it does, then there is an integer solution for 'a'. With this knowledge, we can just iterate through from k = 1 until
+k(k+1) >= 2n, at which point there will be no more solutions.
+*/
+
 int main() {
   int n, n2, ways;
   cin >> n;
